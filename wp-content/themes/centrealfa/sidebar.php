b06d3c610c9c65fe7b9d9f5a5205f304f7184aa9
TanguyScholtes <soultaker720@hotmail.com>
2016-09-01 18:54:01 +0200

    modified sidebar

--- wp-content/themes/centrealfa/sidebar.php
+++ wp-content/themes/centrealfa/sidebar.php
@@ -1,7 +1,4 @@
 <div id="sidebar-primary" class="sidebar">
-    <div id="menuBtn">
-        <img src="img/menu-icon.png" title="Afficher/Cacher le Menu secondaire">
-    </div>
     <div id="myMenu" class="left-column">
         <?php dynamic_sidebar('sidebar-1'); ?>
     </div>
